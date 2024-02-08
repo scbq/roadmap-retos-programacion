49b9e0db46b1e15d7e672bdf8f33b48da6d42a00
scbq <felipe.aguilera.fuentealba@gmail.com>
2024-02-07 22:27:18 -0300

    reto 00

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/scbq.py
@@ -0,0 +1,35 @@
+# https://python.org
+
+# Comentario en una línea
+
+"""
+Esto es un comentario 
+en varias  líneas, que se extiende por todo el bloque de código.
+Puede contener información relevante para entender el programa o explicar su funcionamiento.
+"""
+
+'''
+Esto también es un comentario 
+en varias lineas. Se utiliza entre triples apóstrofes y no tiene la capacidad de ser impreso en pantalla.
+'''
+
+# Variables
+variable = 'Mi variable'
+variable = 'Nuevo valor de mi variable'
+
+# Constante
+MY_CONSTANTE = 'Mi Constante' # Solo por convención ya que no existen constantes en Python
+
+# Datos Primitivos
+int_numero = 2
+float_numero = 1.5
+bool_verdadero = True
+bool_falso = False
+cadena = "Hola Mundo!"
+
+print('!Hola, Python')
+
+print(type(int_numero))
+print(type(float_numero))
+print(type(bool_verdadero))
+print(type(cadena))
